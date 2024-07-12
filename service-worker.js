77ac75a41bbde49b9c732b36498a651055c652ec
Msieur Gab <73283+msieur-gab@users.noreply.github.com>
2024-07-12 18:13:44 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -16,17 +16,26 @@ function startTimer(duration) {
     console.log(`Starting ${duration} second timer`);
     setTimeout(() => {
         console.log('Timer complete');
-        self.registration.showNotification('Tea Timer', {
-            body: 'Your tea is ready!',
-            icon: '/icon.png', // Make sure to have an icon file
-            vibrate: [200, 100, 200]
-        });
-
-        // Notify all clients that the timer is complete
-        self.clients.matchAll().then(clients => {
-            clients.forEach(client => {
-                client.postMessage({action: 'timerComplete'});
-            });
-        });
+        notifyTimerComplete();
     }, duration * 1000);
+}
+
+function notifyTimerComplete() {
+    self.registration.getNotifications().then(notifications => {
+        if (notifications.length === 0) {
+            // Only show a notification if there isn't already one
+            self.registration.showNotification('Tea Timer', {
+                body: 'Your tea is ready!',
+                icon: '/icon.png', // Make sure to have an icon file
+                vibrate: [200, 100, 200]
+            }).catch(error => console.log('Error showing notification:', error));
+        }
+    });
+
+    // Notify all clients that the timer is complete
+    self.clients.matchAll().then(clients => {
+        clients.forEach(client => {
+            client.postMessage({action: 'timerComplete'});
+        });
+    });
 }
