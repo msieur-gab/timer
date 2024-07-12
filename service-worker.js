fc1a92adda1c1e09ac7ca732668cdb7b07734085
Msieur Gab <73283+msieur-gab@users.noreply.github.com>
2024-07-12 18:17:15 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -21,21 +21,25 @@ function startTimer(duration) {
 }
 
 function notifyTimerComplete() {
-    self.registration.getNotifications().then(notifications => {
-        if (notifications.length === 0) {
-            // Only show a notification if there isn't already one
-            self.registration.showNotification('Tea Timer', {
-                body: 'Your tea is ready!',
-                icon: '/icon.png', // Make sure to have an icon file
-                vibrate: [200, 100, 200]
-            }).catch(error => console.log('Error showing notification:', error));
-        }
-    });
-
-    // Notify all clients that the timer is complete
     self.clients.matchAll().then(clients => {
         clients.forEach(client => {
             client.postMessage({action: 'timerComplete'});
         });
     });
+
+    // Check notification permission before showing notification
+    self.registration.pushManager.getSubscription()
+        .then(subscription => {
+            if (subscription) {
+                // Permission was granted, show notification
+                return self.registration.showNotification('Tea Timer', {
+                    body: 'Your tea is ready!',
+                    icon: '/icon.png', // Make sure to have an icon file
+                    vibrate: [200, 100, 200]
+                });
+            } else {
+                console.log('No push subscription, cannot show notification');
+            }
+        })
+        .catch(error => console.log('Error checking push subscription:', error));
 }
