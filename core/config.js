c6b17dbfd34512d01cc0aa361cef3cf9a3a41054
Msieur Gab <73283+msieur-gab@users.noreply.github.com>
2024-07-15 17:32:01 +0200

    Create config.js

---
+++ core/config.js
@@ -0,0 +1,18 @@
+export const APP_VERSION = 'v0.0.9';
+export const DEBUG = false;
+
+export const TIMER_EVENTS = {
+    TIME_UPDATE: 'timeUpdate',
+    TIMER_FINISHED: 'timerFinished',
+    TIMER_START: 'timerStart',
+    TIMER_PAUSE: 'timerPause',
+    TIMER_RESET: 'timerReset',
+    ADD_TIME: 'addTime'
+};
+
+export const UI_EVENTS = {
+    START_TIMER: 'startTimer',
+    PAUSE_TIMER: 'pauseTimer',
+    RESET_TIMER: 'resetTimer',
+    ADD_TIME: 'addTime'
+};
